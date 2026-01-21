8a1db42fc8f55898d2a9234aaa56c4ac1b2ce7d2
Ethan Betts <you@example.com>
2026-01-21 12:54:48 +0100

    checkpoint

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.event_view import EventViewSet
 from .views.pricing_view import calculate_upfront_price
-from .views.preference_views import ColorViewSet, FlowerTypeViewSet
+from .views.color_view import ColorViewSet
+from .views.flower_type_view import FlowerTypeViewSet
 from .views.flower_plan_view import FlowerPlanViewSet
 
 router = DefaultRouter()
